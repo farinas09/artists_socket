759579e8bbe7da98d8cec00601c8c519ca5ccd08
Erick Fariñas <efarinas@Ericks-MacBook-Pro.local>
2021-04-07 23:16:36 -0600

    configure logic for votations

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,17 @@
 const { io } = require("../index");
+const Artist = require("../models/artist");
+const Artists = require("../models/artists");
 
-// socket messages
+const artists = new Artists();
+
+artists.addArtist(new Artist('Drake'));
+artists.addArtist(new Artist('Ed Sheeran'));
+artists.addArtist(new Artist('Travis Scott'));
 
 io.on("connection", (client) => {
   console.log("Client Connected");
+
+  client.emit('active-artists', artists.getArtists());
 
   client.on("addArtist", (data) => {
     client.broadcast.emit("addArtist", { artist: data });
